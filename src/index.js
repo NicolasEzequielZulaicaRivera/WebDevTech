ad894d8f2cedd6b7fa5ab22907f60fbe3a5850e4
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-08-08 22:29:20 -0300

    wrapping up

--- src/index.js
+++ src/index.js
@@ -2,27 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
+function Square(props){//'function component' . no state, only render . simplified notation
+  return (
+      <button
+        className="square" 
+        onClick={props.onClick}
+      >
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
+  
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.props.squares[i]} 
+        onClick={()=>this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -44,19 +49,109 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [
+        { squares:Array(9).fill(null), },
+      ],
+      xIsNext: true,
+      moveNumber: 0,
+    }
+  }
+  handleClick(i){
+    const history = this.state.history.slice(
+        0,this.state.moveNumber +1
+      );
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();//shallow copy. Data change without mutation is preferable (keep previous states, detect change).
+    
+    if( calculateWinner(squares) || squares[i]){
+      return;
+    }
+
+
+    squares[i] = this.state.xIsNext?'X':'O';//this doesnt alter the state as strings are passed by value on shallow-copy (so are ints and bools)
+    this.setState( 
+      {
+        history: history.concat([
+          {squares: squares},
+        ]),
+        moveNumber: history.length,
+        xIsNext: !this.state.xIsNext,
+      } 
+    );//not a huge fan of refreshing the whole board -> shouldComponentUpdate tackles my worries for components on more complex setups
+  }
+  jumpTo(moveNumber){
+    this.setState({
+      moveNumber: moveNumber,
+      xIsNext: (moveNumber%2)===0,
+    })
+  }
   render() {
+    const history = this.state.history;
+    const current = history[this.state.moveNumber];
+    const winner = calculateWinner(current.squares);
+
+    const historyButtons = history.map(
+      (step, moveNumber) => {
+        const description = moveNumber?
+          'Go to move #'+moveNumber :
+          'Start Game';
+        return(
+          <li key={moveNumber}>
+            <button 
+              onClick={ ()=>{this.jumpTo(moveNumber)} }
+            >
+              {description}
+            </button>
+          </li>
+        );
+      }
+    );
+
+    let status;
+    if(winner){
+      status = 'Winner: '+ winner;
+    } else {
+      status = 'Next player: '+(this.state.xIsNext?'X':'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick = {(i)=>this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{historyButtons}</ol>
         </div>
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
