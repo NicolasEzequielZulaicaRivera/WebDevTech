98221dff672493aa3b9244a65fb9fa4657d72ffa
Nicolas Zulaica <nzulaica@fi.uba.ar>
2021-09-29 15:17:01 -0300

    Getting Started

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from "@apollo/client";
+
+const USD_EXCHANGE_RATES_QUERY = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+const ExchangeRates = () => {
+  const { loading, error, data } = useQuery(USD_EXCHANGE_RATES_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>USD EXCHANGE RATES</h3>
+        <ExchangeRates />
       </header>
     </div>
   );
