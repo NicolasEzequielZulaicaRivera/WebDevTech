226c8d0e9c3588471cef69cad2a17acb2707af4d
nicolaszulaica <nzulaica@fi.uba.ar>
2021-12-30 17:12:51 -0300

    App Setup

--- tstemp/src/App.tsx
+++ tstemp/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Counter, AltCounter} from './components/Counter';
+import {Tasks} from './components/Tasks';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <section className={'counters'}>
+        <Counter />
+        <AltCounter />
+      </section>
+      <hr />
+      <section className={'tasks'}>
+        <Tasks />
+      </section>
     </div>
   );
 }
