0f4febe41ea8f1915883c7377f9288f76088aa20
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-03-25 15:06:55 -0300

    mocked api

--- basic_restapi/src/routes/characters.js
+++ basic_restapi/src/routes/characters.js
@@ -3,11 +3,11 @@ const router = Router();
 
 let characters = [
   {
-    id: "0",
+    id: 0,
     name: "mickey",
   },
   {
-    id: "1",
+    id: 1,
     name: "minnie",
   },
 ];
@@ -18,12 +18,47 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params?.id);
+
+  if (!id)
+    return res.status(400).json({ error: `Invalid id: '${req.params.id}'` });
+
+  const character = characters.find((char) => char.id === parseInt(id));
+
+  if (!character)
+    return res
+      .status(500)
+      .json({ error: `Character with { id: ${id} } Not Found` });
 
   res.json({
-    status: "TBD",
+    data: character,
   });
+});
+
+router.post("/", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) return res.status(400).json({ error: `Missing field: 'name'` });
+
+  if (characters.some((char) => char.name === name))
+    return res.status(500).json({ error: `Duplicated name: '${name}'` });
+
+  // find current max id
+  const id =
+    characters.reduce((max, char) => {
+      if (char.id > max) max = char.id;
+      return max;
+    }, 0) + 1;
+
+  const newCharacter = {
+    id,
+    name,
+  };
+
+  characters.push(newCharacter);
+
+  res.json({ data: newCharacter });
 });
 
 module.exports = router;
